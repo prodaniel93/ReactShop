c11b2c4fdf728cbb977f6e926eb649d2ecbd0d61
HwangJongSoo <ghkdwhdtn9@naver.com>
2023-01-01 21:59:58 +0900

    3. 사진 넣어보기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import bg from './img/bg.png';
 
 function App() {
   return (
     <div className='App'>
-      <Button variant='primary'>Primary</Button>
+      <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand href='#home'>ShoeShop</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div
+        className='main-bg'
+        style={{ backgroundImage: 'url(' + bg + ')' }}
+      ></div>
     </div>
   );
 }
