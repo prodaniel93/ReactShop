11354a4a1f9217b71ff23d17c60eaeff132f95b6
HwangJongSoo <ghkdwhdtn9@naver.com>
2023-01-04 01:37:17 +0900

    state 변경 + createSlice + configureStore

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,24 +1,47 @@
 import { Table } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeName } from './../store';
 
 function Cart() {
+  let state = useSelector((state) => {
+    return state;
+  });
+  //   console.log(state);
+
+  let dispatch = useDispatch();
+
   return (
     <div>
+      {state.user}의 장바구니
       <Table>
         <thead>
           <tr>
             <th>#</th>
             <th>상품명</th>
             <th>수량</th>
-            <th>변경하기</th>
+            <td>버튼</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {state.cart.map((a, i) => {
+            return (
+              <tr key={i}>
+                <td>{state.cart[i].id}</td>
+                <td>{state.cart[i].name}</td>
+                <td>{state.cart[i].count}</td>
+
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(changeName());
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
