8144c01a05d58ff3f45a5d4b5853d260845a84ea
HwangJongSoo <ghkdwhdtn9@naver.com>
2023-01-03 15:23:00 +0900

    1. styled-component

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,18 +1,30 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+  background: yellow;
+  color: black;
+  padding: 10px;
+`;
 
 function Detail(props) {
   let { id } = useParams();
+  let findProduct = props.shoes.find(function (x) {
+    return x.id == id;
+  });
 
   return (
     <div className='container'>
+      <YellowBtn>버튼</YellowBtn>
+
       <div className='row'>
         <div className='col-md-6'>
           <img src='/image/shoes1.jpg' width='100%' />
         </div>
         <div className='col-md-6'>
-          <h4 className='pt-5'>{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <h4 className='pt-5'>{findProduct.title}</h4>
+          <p>{findProduct.content}</p>
+          <p>{findProduct.price}</p>
           <button className='btn btn-danger'>주문하기</button>
         </div>
       </div>
